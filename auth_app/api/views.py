518674273ad0ce43ff9024cf8114924f8f9109d5
Kevin Hase <hasekevin21@googlemail.com>
2026-06-10 15:37:20 +0200

    add imports

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from auth_app.api.serializer import RegisterSerializer
 
 # Create your views here.
 
 class RegisterView(APIView):
-    def post(self,request):
+    def post(self, request):
         serializer = RegisterSerializer(data=request.data)
