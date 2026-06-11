1590d3d39b97b09f25956c4bfe017cadd584a792
Kevin Hase <hasekevin21@googlemail.com>
2026-06-11 14:05:37 +0200

    add serializer

--- auth_app/api/serializer.py
+++ auth_app/api/serializer.py
@@ -1,22 +1,62 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+GENERIC_ERROR = "Please check your input and try again."
+
 
 class RegisterSerializer(serializers.ModelSerializer):
+    confirmed_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['email', 'password', 'confirm_password']
+        fields = ['email', 'password', 'confirmed_password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(GENERIC_ERROR)
+        return value
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirmed_password']:
+            raise serializers.ValidationError({'confirmed_password': "Passwords do not match."})
+        validate_password(attrs['password'])
+        return attrs
+
     def create(self, validated_data):
+        validated_data.pop('confirmed_password')
         user = User.objects.create_user(
-            username=validated_data['email'].split('@')[0],
+            username=validated_data['email'],
             email=validated_data['email'],
             password=validated_data['password'],
-            confirm_password=validated_data['confirm_password']
+            is_active=False,
         )
         return user
 
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
     def validate(self, attrs):
-        if attrs['password'] != attrs['confirm_password']:
+        user = User.objects.filter(email=attrs['email']).first()
+        if user is None or not user.check_password(attrs['password']) or not user.is_active:
+            raise serializers.ValidationError(GENERIC_ERROR)
+        attrs['user'] = user
+        return attrs
+
+
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class PasswordConfirmSerializer(serializers.Serializer):
+    new_password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['confirm_password']:
             raise serializers.ValidationError({'confirm_password': "Passwords do not match."})
+        validate_password(attrs['new_password'])
         return attrs
