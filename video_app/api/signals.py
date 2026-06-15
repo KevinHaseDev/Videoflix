e811b102bd98c8dd3df173ff74edb0b3922fc7eb
Kevin Hase <hasekevin21@googlemail.com>
2026-06-15 20:41:46 +0200

    fix signals and urls

--- video_app/api/signals.py
+++ video_app/api/signals.py
@@ -7,15 +7,21 @@ from django.conf import settings
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from video_app.api.service import convert_video_to_hls
+from video_app.api.service import convert_video_to_hls, extract_thumbnail
 from video_app.models import Video
 
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    """Enqueue HLS conversion when a new video is uploaded."""
-    if created:
-        django_rq.get_queue('default').enqueue(convert_video_to_hls, instance.pk)
+    """Enqueue thumbnail extraction and HLS conversion when a new video is uploaded.
+
+    Both jobs read the uploaded video file, so they are only enqueued when a
+    file is actually present; otherwise ffmpeg would fail on an empty path.
+    """
+    if created and instance.video_file:
+        queue = django_rq.get_queue('default')
+        queue.enqueue(extract_thumbnail, instance.pk)
+        queue.enqueue(convert_video_to_hls, instance.pk)
 
 
 @receiver(post_delete, sender=Video)
