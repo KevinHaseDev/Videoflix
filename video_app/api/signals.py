d335299b755a0b0f81a020fec0f080b113ee25ca
Kevin Hase <hasekevin21@googlemail.com>
2026-06-15 12:24:28 +0200

    enqueue hls conversion on upload

--- video_app/api/signals.py
+++ video_app/api/signals.py
@@ -1,19 +1,30 @@
 """signals for the video app."""
-from ..models import Video
+import shutil
+from pathlib import Path
+
+import django_rq
+from django.conf import settings
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
-import os
+
+from video_app.api.service import convert_video_to_hls
+from video_app.models import Video
+
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    """Signal handler for post_save of Video model."""
-    print("Video post save signal triggered")
+    """Enqueue HLS conversion when a new video is uploaded."""
     if created:
-        print("New video created")
+        django_rq.get_queue('default').enqueue(convert_video_to_hls, instance.pk)
+
 
 @receiver(post_delete, sender=Video)
 def video_delete_signal(sender, instance, **kwargs):
-    """Signal handler for post_delete of Video model."""
+    """Delete the video file and HLS output directory on deletion."""
     if instance.video_file:
-        if os.path.isfile(instance.video_file.path):
-            os.remove(instance.video_file.path)
+        video_path = Path(instance.video_file.path)
+        if video_path.is_file():
+            video_path.unlink()
+    hls_dir = settings.MEDIA_ROOT / "videos" / str(instance.pk)
+    if hls_dir.is_dir():
+        shutil.rmtree(hls_dir)
