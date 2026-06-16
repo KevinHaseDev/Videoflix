d13ecf8c68b67b94afd6a2ba9052c16282ad77ea
Kevin Hase <hasekevin21@googlemail.com>
2026-06-16 12:58:54 +0200

    delete useless model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,17 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 # Create your models here.
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    token = models.CharField(max_length=255, blank=True, null=True)
-    is_activated = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.user.username
-    
-    class Meta:
-        verbose_name = 'Profile'
-        verbose_name_plural = 'Profiles'
-    
